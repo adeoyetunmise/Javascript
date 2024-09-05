4add74f97e6c1dfc293556803d109d4d35efa797
adeoyetunmise <adeoyetunmise53@gmail.com>
2024-09-05 13:19:01 +0100

    worked on function today

---
+++ function.js
@@ -0,0 +1,60 @@
+// function greetings(user="user"){
+// console.log(user);
+
+// }
+// greetings()
+
+// const salary = 170000;
+
+// function getVAT(pay){
+//     const tax = pay * 0.075
+
+//     return tax
+// }
+
+// const VAT = getVAT(salary)
+
+// const net = salary - VAT
+
+// console.log(net);
+
+
+// // Parameter
+
+// const user = prompt("Enter your Name:")
+
+// function greet(person){
+//     console.log("Welcome" + person);
+    
+// }
+
+// greet(user)
+
+// const admin = confirm("are you an admin")
+
+// if(admin){
+//     const name = prompt("what is your admin name?")
+//     greet(name)
+// }
+
+
+//  return statement
+
+// function isOdd(num){
+//     if (num % 2 === 0){
+//         return num + "is even"
+//     }
+
+//     return + "is Odd"
+// }
+
+// alert(isOdd(10))
+
+const result = prompt("write Whatever")
+
+function capitalLetter(){
+    console.log(result.toUpperCase());
+    
+    
+}
+capitalLetter()
