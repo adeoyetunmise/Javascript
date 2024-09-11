c90fbd8e3ed10ec339354bfc4b2fad2ce22d8bc3
adeoyetunmise <adeoyetunmise53@gmail.com>
2024-09-11 10:24:33 +0100

    learning Dom

---
+++ DOM.js
@@ -0,0 +1,6 @@
+const register = document.querySelector('#register');
+const message = querySelector('.msg');
+
+register.computedStyleMap.color = 'red'
+
+message.innerText = 'Please input your valid information'
